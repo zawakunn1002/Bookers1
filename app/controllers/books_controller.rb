29f366a2a63e6aabfea5a733dafd0e80b8b6422f
daichi <bumpk1002@gmail.com>
2019-05-12 21:53:11 +0900

    [update]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+  def index
+  	@posts = Book.all
+  	@book = Book.new
+
+  end
+
+  def create
+  	  book = Book.new(book_params)
+  	  book.save
+  	  flash[:notice] ="Book was successfully created"
+  	  redirect_to book_path(book.id)
+
+
+  end
+
+  def show
+  	 @book = Book.find(params[:id])
+  end
+
+  def edit
+  	  @book = Book.find(params[:id])
+
+  end
+
+  def update
+  	  book = Book.find(params[:id])
+  	  book.update(book_params)
+  	  redirect_to book_path(book.id)
+  end
+
+  def destroy
+  	  book = Book.find(params[:id])
+  	  book.destroy
+  	  redirect_to books_path
+
+  end
+
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :books
+
+  root 'home#index'
+end
